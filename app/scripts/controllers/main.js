3bf1ac4d8506e929d2cc3ed1f4b624d18d84eb26
sebastiaanstuij <sebastiaan.stuij@gmail.com>
2014-09-20 13:36:10 +0200

    fixed grunt usemin and changed image filenames

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,17 +13,17 @@ angular.module('websiteMelanieApp')
         $scope.myInterval = 5000;
         var slides = $scope.slides = [
             {
-                image: '/images/slide1/CarouselLargeM&R.jpg',
+                image: '../images/slide-1/carousel-large-mr.jpg',
                 title: 'Engelse conversatie lessen met Melanie',
                 subtitle: 'Leer op een leuke en informele manier vloeiend Engels spreken'
             },
             {
-                image: '..\\images\\slide_2\\Carousel_Large_M&H.jpg',
+                image: '../images/slide-2/carousel-large-mh.jpg',
                 title: 'Individuele lessen',
                 subtitle: 'Individuele coaching, zodat u snel progressie boekt'
             },
             {
-                image: '..\\images\\slide_3\\Carousel_Large_M&C&I.jpg',
+                image: '../images/slide-3/carousel-large-mci.jpg',
                 title: 'Groepslessen',
                 subtitle: 'Ook groepslessen behoren tot te mogelijkheden'
             }
