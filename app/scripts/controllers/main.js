f54192674a32b8583b018f9191a7557724fe0f8e
sebastiaanstuij <sebastiaan.stuij@gmail.com>
2014-09-20 13:40:20 +0200

    fixed grunt usemin and changed image filenames

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,7 +13,7 @@ angular.module('websiteMelanieApp')
         $scope.myInterval = 5000;
         var slides = $scope.slides = [
             {
-                image: '../images/slide-1/carousel-large-mr.jpg',
+                image: 'url(/images/slide-1/carousel-large-mr.jpg)',
                 title: 'Engelse conversatie lessen met Melanie',
                 subtitle: 'Leer op een leuke en informele manier vloeiend Engels spreken'
             },
