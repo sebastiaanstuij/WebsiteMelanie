5d0807a8d1b993fad161d5239bfde66a7fc801e3
sebastiaanstuij <sebastiaan.stuij@gmail.com>
2014-11-13 23:01:52 +0100

    google-map directive problem

--- app/scripts/directives/googleMapDirective.js
+++ app/scripts/directives/googleMapDirective.js
@@ -1,63 +1,23 @@
-angular.module('websiteMelanieApp', []).
+angular.module('websiteMelanieApp').
     directive('googleMap', function() {
-        // directive link function
-        console.log("Directive was run");
+      return {
+        restrict: 'E',
+        link: function (scope, elem, attrs) {
+          console.log('poep');
+          var mapOptions,
+            latitude = attrs.latitude,
+            longitude = attrs.longitude,
+            map;
 
-        var link = function(scope, element, attrs) {
-            var map, infoWindow;
-            var markers = [];
+          latitude = latitude && parseFloat(latitude, 10) || 43.074688;
+          longitude = longitude && parseFloat(longitude, 10) || -89.384294;
 
-            // map config
-            var mapOptions = {
-                center: new google.maps.LatLng(53.103136, 6.085389),
-                zoom: 4,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                scrollwheel: false
-            };
+          mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(latitude, longitude)
+          };
 
-            // init the map
-            function initMap() {
-                if (map === void 0) {
-                    map = new google.maps.Map(element[0], mapOptions);
-                }
-            }
-
-            // place a marker
-            function setMarker(map, position, title, content) {
-                var marker;
-                var markerOptions = {
-                    position: position,
-                    map: map,
-                    title: title,
-                    icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
-                };
-
-                marker = new google.maps.Marker(markerOptions);
-                markers.push(marker); // add marker to array
-
-                google.maps.event.addListener(marker, 'click', function () {
-                    // close window if not undefined
-                    if (infoWindow !== void 0) {
-                        infoWindow.close();
-                    }
-                    // create new window
-                    var infoWindowOptions = {
-                        content: content
-                    };
-                    infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-                    infoWindow.open(map, marker);
-                });
-            }
-
-            // show the map and place some markers
-            initMap();
-            setMarker(map, new google.maps.LatLng(53.103136, 6.085389), 'Drachten', 'Hier geeft Melanie les');
-        };
-
-        return {
-            restrict: 'A',
-            template: '<div id="gmap"></div>',
-            replace: true,
-            link: link
-        };
+          map = new google.maps.Map(elem[0], mapOptions);
+        }
+      };
     });
