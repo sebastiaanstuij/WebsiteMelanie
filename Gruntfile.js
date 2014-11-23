3a1fb3a2e1697e1bf5fc4e000e22c3be3ffb5a5b
sebastiaanstuij <sebastiaan.stuij@gmail.com>
2014-11-23 15:44:36 +0100

    added SEO changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -373,6 +373,22 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
+    },
+
+    // Grunt build control settings
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:sebastiaanstuij/WebsiteMelanie.git',
+          branch: 'gh-pages'
+        }
+      }
     }
   });
 
