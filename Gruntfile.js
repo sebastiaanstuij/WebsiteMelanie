f4996021d6c0c7969a042273030e843dbc9e0bfb
sebastiaanstuij <sebastiaan.stuij@gmail.com>
2014-11-24 11:55:45 +0100

    modified about page and nojekyll file added

--- Gruntfile.js
+++ Gruntfile.js
@@ -174,7 +174,7 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the app
     wiredep: {
       options: {
-        //cwd: '<%= yeoman.app %>'
+        cwd: '<%= yeoman.app %>'
       },
       app: {
         src: ['<%= yeoman.app %>/index.html'],
@@ -331,7 +331,8 @@ module.exports = function (grunt) {
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'fonts/*',
-            'CNAME'
+            'CNAME',
+            'views/.nojekyll'
           ]
         }, {
           expand: true,
--- app/scripts/directives/googleMapDirective.js
+++ app/scripts/directives/googleMapDirective.js
@@ -3,20 +3,45 @@ angular.module('websiteMelanieApp').
       return {
         restrict: 'C',
         link: function (scope, elem, attrs) {
-          var mapOptions,
-            latitude = attrs.latitude,
-            longitude = attrs.longitude,
-            map;
+          var mapOptions, markerOptions, map, marker, infoWindow;
 
-          latitude = latitude && parseFloat(latitude, 10) || 43.074688;
-          longitude = longitude && parseFloat(longitude, 10) || -89.384294;
+          latitude = 53.106021;
+          longitude = 6.103242;
 
+          // map config
           mapOptions = {
             zoom: 8,
-            center: new google.maps.LatLng(latitude, longitude)
+            center: new google.maps.LatLng(latitude, longitude),
+            scrollwheel: false
           };
 
           map = new google.maps.Map(elem[0], mapOptions);
+
+          // marker config
+          markerOptions = {
+            position: new google.maps.LatLng(latitude, longitude),
+            map: map,
+            title: 'Drachten, Friesland',
+            icon: 'https://maps.google.com/mapfiles/ms/icons/green-dot.png'
+          };
+
+
+
+          marker = new google.maps.Marker(markerOptions);
+
+
+          google.maps.event.addListener(marker, 'click', function () {
+            // close window if not undefined
+            if (infoWindow !== void 0) {
+              infoWindow.close();
+            }
+            // create new window
+            var infoWindowOptions = {
+              content: 'Drachten, Friesland'
+            };
+            infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+            infoWindow.open(map, marker);
+          });
         }
       };
     });
